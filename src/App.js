3e151193dd5e9170204b4f98eb42c39b413fa594
Jack McKelvey <71670664+McKelvey-Jack@users.noreply.github.com>
2021-02-08 14:31:22 +0000

    fix opacity issue on chrome

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import "./App.css";
-import { Router } from "@reach/router";
-import Hero from "./components/Hero";
-import StadiumsList from "./components/StadiumsList";
-import IndividualStadium from './components/IndividualStadium'
+import './App.css';
+import { Router } from '@reach/router';
+import Hero from './components/Hero';
+import StadiumsList from './components/StadiumsList';
+import IndividualStadium from './components/IndividualStadium';
 
 function App() {
   return (
-      <div className="App">
-        <Router>
-          <Hero path="/" />
-
-          <StadiumsList path="/countries/:country" />
-          <IndividualStadium path="/stadiums/:stadiumId" />
-        </Router>
-      </div>
+    <div className="App">
+      <Router>
+        <Hero path="/" />
+        <StadiumsList path="/countries/:country" />
+        <IndividualStadium path="/stadiums/:stadiumId" />
+      </Router>
+    </div>
   );
 }
 
